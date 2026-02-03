d0c0684c30a1af763a90440947c30311af241d63
Askir4 <lionsin43@gmail.com>
2026-02-03 12:40:10 +0100

    Fixed Problem by moving user input
    By moving the user input under the 2. while true and using the relevant function, the problem was fixed.

--- Python/Password_Generator/Generator.py
+++ Python/Password_Generator/Generator.py
@@ -1,8 +1,9 @@
 #Simple Password Generator
 #DO NOT USE FOR REAL PASSWORDS
 #This is for educational purposes only
-print("##Password Generator##")
-
+print("\n##Password Generator##\n")
+#Ask user to enter the length of the password
+#Define user input is Length_Password
 while True: 
     Length_Password=input("Please enter the length of the password: ")
     Length_Password=Length_Password.strip()
@@ -14,11 +15,14 @@ while True:
 print("1:I want my password to only contain letters and numbers")
 print("2:I want my password to contain only numbers")
 print("3I want my password to contain letters, numbers and symbols")
-Complexity_Password=input("Please choose the complexity of your password:")
-Complexity_Password=Complexity_Password.strip().lower()
+
 
 while True:
-    if Complexity_Password=="1" or Complexity_Password=="2" or Complexity_Password=="3":
+    #Ask user to enter the complexity of the password
+    #Define user input is Complexity_Password
+    Complexity_Password=input("Please choose the complexity of your password:")
+    Complexity_Password=Complexity_Password.strip().lower()
+    if Complexity_Password in ["1","2","3"]:
         break
     else:
-        print("Please enter a valid option (choose 1,2 or 3)")
+        print("\n Please enter a valid option (choose 1,2 or 3)\n")

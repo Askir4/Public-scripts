3531016057114913165f64fb8fde4bacdfe785be
Askir4 <lionsin43@gmail.com>
2026-02-03 12:22:59 +0100

    Add Option chooser
    -Buggy (have to fix endless Loop if wrong option is selected

--- Python/Password_Generator/Generator.py
+++ Python/Password_Generator/Generator.py
@@ -10,3 +10,15 @@ while True:
         break
     else:
         print ("Please enter a number greater than 0.")
+
+print("1:I want my password to only contain letters and numbers")
+print("2:I want my password to contain only numbers")
+print("3I want my password to contain letters, numbers and symbols")
+Complexity_Password=input("Please choose the complexity of your password:")
+Complexity_Password=Complexity_Password.strip().lower()
+
+while True:
+    if Complexity_Password=="1" or Complexity_Password=="2" or Complexity_Password=="3":
+        break
+    else:
+        print("Please enter a valid option (choose 1,2 or 3)")

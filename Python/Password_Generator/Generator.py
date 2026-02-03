67af0150a37816a57328067a5a7cfd0297445f58
Askir4 <lionsin43@gmail.com>
2026-02-03 12:07:36 +0100

    Add Length Question
    -First Part of the programm

--- Python/Password_Generator/Generator.py
+++ Python/Password_Generator/Generator.py
@@ -3,4 +3,10 @@
 #This is for educational purposes only
 print("##Password Generator##")
 
-    
+while True: 
+    Length_Password=input("Please enter the length of the password: ")
+    Length_Password=Length_Password.strip()
+    if Length_Password.isnumeric and Length_Password.isdigit() and Length_Password !="0":
+        break
+    else:
+        print ("Please enter a number greater than 0.")

f19d29ef90164fb2d4de04839c4e1bc742130daa
Askir4 <lionsin43@gmail.com>
2026-02-03 13:04:32 +0100

    Add Documentation and Disclaimers

--- Python/Password_Generator/Generator.py
+++ Python/Password_Generator/Generator.py
@@ -1,16 +1,17 @@
+#Since this is an educational project for my self, I followed tutorials (both written and on YouTube) to create this tool.
+#Since I am a beginner basically every bit is commented as some kind of documentation for me.(How does is work?:)
 #Simple Password Generator
 #DO NOT USE FOR REAL PASSWORDS
 #This is for educational purposes only
-print("\n##Password Generator##\n")
-#Ask user to enter the length of the password
-#Define user input is Length_Password
+print("\n##Password Generator##\n") #Print the title of the tool in a new line by using \n
+
 while True: 
     Length_Password=input("Please enter the length of the password: ")
-    Length_Password=Length_Password.strip()
-    if Length_Password.isnumeric and Length_Password.isdigit() and Length_Password !="0":
-        break
+    Length_Password=Length_Password.strip()#Remoce white spaces (for example spacebar inputs by user)
+    if Length_Password.isnumeric and Length_Password.isdigit() and Length_Password !="0": #Check if the input given by the user is a number and not 0.ö
+        break #Breaks the current loop if the conditions are true
     else:
-        print ("Please enter a number greater than 0.")
+        print ("Please enter a number greater than 0.")#inform user that the input is not valid
 
 print("1:I want my password to only contain letters and numbers")
 print("2:I want my password to contain only numbers")
@@ -21,8 +22,8 @@ while True:
     #Ask user to enter the complexity of the password
     #Define user input is Complexity_Password
     Complexity_Password=input("Please choose the complexity of your password:")
-    Complexity_Password=Complexity_Password.strip().lower()
-    if Complexity_Password in ["1","2","3"]:
+    Complexity_Password=Complexity_Password.strip()#Remoce white spaces (for example spacebar inputs by user)
+    if Complexity_Password in ["1","2","3"]:#Give a list of valid options.by using in operator.
         break
     else:
         print("\n Please enter a valid option (choose 1,2 or 3)\n")
--- Python/Password_Generator/README.md
+++ Python/Password_Generator/README.md
@@ -1,8 +1,13 @@
 ### Password Generator
 
 Dieses Tool wird rein als Übung geschrieben und dient dazu, ein zufälliges Passwort zu generieren.
-
+---
 ### Disclaimer
 
 Dieses Projekt dient rein als Übung. Es wird keine Garantie für die Sicherheit oder Funktionalität gegeben.Es wird von der Verwendung abgeraten, da der Code potenziell voller Fehler ist.
+---
+### Hinweise
 
+Hier werden sehr viele Kommentare verwendet, da ich versuche die Logik des Codes für mich selbst zu dokumentieren, da ich hier tutorials folge, um Python zu lernen.
+
+---
